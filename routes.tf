c95fc13bc4a197fe72865f6327e489e7ae60e4ee
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-28 08:36:01 +0530

    Update terraform-modules/terraform-vpc/

--- routes.tf
+++ routes.tf
@@ -17,3 +17,9 @@ resource "aws_route" "route-from-default-vpc" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-to-default-vpc.id
 }
 
+resource "aws_route" "route-for-igw" {
+  route_table_id         = aws_route_table.route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
