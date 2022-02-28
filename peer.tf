03fe85f1b86921104a8f6326053cb1931474674e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-28 08:15:12 +0530

    Update terraform-modules/terraform-vpc/

--- peer.tf
+++ peer.tf
@@ -1,5 +1,6 @@
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "peer-to-default-vpc" {
   peer_vpc_id = var.DEFAULT_VPC_ID
   vpc_id      = aws_vpc.main.id
+  auto_accept = true
 }
 
