3f13d7a40d534e7a65de69fc027851a23da67e15
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-28 08:47:08 +0530

    Update terraform-modules/terraform-vpc/

--- subnets.tf
+++ subnets.tf
@@ -1,18 +1,16 @@
 resource "aws_subnet" "public-subnets" {
-  count             = var.SUBNETS
+  count             = var.PUBLIC_SUBNETS
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.VPC_CIDR, 1, count.index)
+  cidr_block        = cidrsubnet(var.VPC_CIDR, 3, count.index)
   availability_zone = element(var.AZ, count.index)
 }
 
 resource "aws_subnet" "private-subnets" {
-  count             = var.SUBNETS
+  count             = var.PRIVATE_SUBNETS
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.VPC_CIDR, 1, count.index)
+  cidr_block        = cidrsubnet(var.VPC_CIDR, 3, count.index + 2)
   availability_zone = element(var.AZ, count.index)
 }
-
-
 
 resource "aws_route_table_association" "rt-assoc" {
   count          = var.SUBNETS
