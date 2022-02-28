fa6385f102602404be8b736d8fc3875ec58dbdd8
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-28 08:58:10 +0530

    Update terraform-modules/terraform-vpc/

--- gateways.tf
+++ gateways.tf
@@ -6,3 +6,16 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_eip" "public" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.public.id
+  subnet_id     = aws_subnet.public-subnets.*.id[0]
+
+  tags = {
+    Name = "NAT-gw"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
