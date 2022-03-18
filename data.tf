d6905eda904e8bc18756d59caeba2e31ecd2b0ef
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-18 08:20:39 +0530

    Update terraform-modules/terraform-vpc/

--- data.tf
+++ data.tf
@@ -2,3 +2,8 @@ data "aws_route53_zone" "internal" {
   zone_id      = "Z01368741LHGOO0T1BT54"
   private_zone = true
 }
+
+data "aws_route53_zone" "public" {
+  zone_id      = "Z077856535D0FCPZ3R8B"
+  private_zone = false
+}
--- outputs.tf
+++ outputs.tf
@@ -38,3 +38,11 @@ output "INTERNAL_HOSTED_ZONE_NAME" {
   value = data.aws_route53_zone.internal.name
 }
 
+output "PUBLIC_HOSTED_ZONEID" {
+  value = data.aws_route53_zone.public.zone_id
+}
+
+output "PUBLIC_HOSTED_ZONE_NAME" {
+  value = data.aws_route53_zone.public.name
+}
+
